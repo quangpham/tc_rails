6ad08f20585f40a703a5d36fee1164394cf4b892
Quang Pham <me@quangpham.com>
2013-04-04 11:13:31 +0300

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TcRails::Application.config.secret_token = 'e1b1468802004fabf999b4e51265c8e7d584241c36bac9db9e7bb96bdfc06e9fe983ed7abf41dc6c54bbe9c3446a1100ecef8b797a258d2d3f905d7a69911107'
