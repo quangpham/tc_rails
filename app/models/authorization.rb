5ae21ab0eef37a6ac93f71c3ffe282935557f247
Quang Pham <me@quangpham.com>
2013-04-05 13:38:58 +0300

    remove authorizatio

--- app/models/authorization.rb
+++
@@ -1,7 +0,0 @@
-class Authorization < ActiveRecord::Base
-  belongs_to :user
-  validates_presence_of :user_id, :uid, :provider
-  validates_uniqueness_of :uid, :scope => :provider
-  
-  attr_accessible :provider, :uid, :user_id
-end
