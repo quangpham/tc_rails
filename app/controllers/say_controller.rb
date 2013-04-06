7b842ee016fb8d9309c7fe06df7f3d822b0b96e1
Quang Pham <me@quangpham.com>
2013-04-06 22:51:22 +0300

    Pre fill data from Linkedin

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -8,6 +8,10 @@ class SayController < ApplicationController
     
     @company_search_results = client.search({ :keywords => "Activeark"}, "company").companies.all
     #raise @company_search_results.to_yaml
+    
+    @com_data = client.company(:id => 2393260, :fields => %w{ id name email-domains industry logo-url specialties description locations:(address:(city state country-code) is-headquarters) employee-count-range })
+    # current_user.companies.create(:linkedin_id => com_data.id, :name => com_data.name, :description => com_data.description)
+    # raise @com_data.to_yaml
   end
 
   def goodbye
