15a439ac5d0ea6463c83813ed5beb7d3c5f80573
Quang Pham <me@quangpham.com>
2013-04-06 22:53:35 +0300

    .

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -6,10 +6,10 @@ class SayController < ApplicationController
     @search_results = client.search(:keywords => "Activeark").people.all
     #raise @search_results.to_yaml
     
-    @company_search_results = client.search({ :keywords => "Activeark"}, "company").companies.all
+    @company_search_results = client.search({ :keywords => "Introdex"}, "company").companies.all
     #raise @company_search_results.to_yaml
     
-    @com_data = client.company(:id => 2393260, :fields => %w{ id name email-domains industry logo-url specialties description locations:(address:(city state country-code) is-headquarters) employee-count-range })
+    @com_data = client.company(:id => @company_search_results.first.id, :fields => %w{ id name email-domains industry logo-url specialties description locations:(address:(city state country-code) is-headquarters) employee-count-range })
     # current_user.companies.create(:linkedin_id => com_data.id, :name => com_data.name, :description => com_data.description)
     # raise @com_data.to_yaml
   end
